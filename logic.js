6c00981d75ae5b8a0bdcd1e119a3ecadf5894104
Ayush Kothari <ayush.kotharift@gmail.com>
2023-03-27 12:56:21 +0530

    Created a "Rock, Paper or Scissors" game which plays 5 rounds and displays the score when any player reaches 3 points. Game sheet is also logged in console

---
+++ logic.js
@@ -0,0 +1,53 @@
+let playerScore = 0;
+let computerScore = 0;
+let gameSheet = [];
+
+function getComputerChoice(){
+    let options = ["Rock","Paper","Scissors"];
+    const randIndex =  Math.floor(Math.random() * options.length);
+    const randChoice = options[randIndex];
+    return randChoice
+}
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    gameSheet.push(`Player Choice : ${playerSelection} | ${computerSelection} : Computer Choice`);
+    if(playerSelection == "rock" && computerSelection == "scissors"){
+        playerScore += 1;
+        return ("You win! Rock beats Scissors");
+    } else if (playerSelection == "rock" && computerSelection == "paper"){
+        computerScore += 1;
+        return ("You lose! Paper beats Rock");
+    } else if (playerSelection == "scissors" && computerSelection == "paper"){
+        playerScore += 1;
+        return ("You win! Scissors cut Paper");
+    } else if (playerSelection == "scissors" && computerSelection == "rock"){
+        computerScore += 1;
+        return ("You lose! Rock beats Scissors");
+    } else if (playerSelection == "paper" && computerSelection == "rock"){
+        playerScore += 1;
+        return("You Win! Paper beats Rock");
+    } else if (playerSelection == "paper" && computerSelection == "scissors"){
+        computerScore += 1;
+        return("You lose! Scissors cut paper");
+    }else {
+        return("Play Again! It's a tie.");
+    }
+    
+}
+
+function game(){
+    let gameCount = 0;
+    while(playerScore + computerScore != 5){
+        let playerSelection = prompt("Rock, Paper or Scissors?");
+        let computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+        gameCount += 1;
+    }
+    alert(`Player Score : ${playerScore} | Computer Score : ${computerScore}`);
+    console.log(gameSheet);
+    return("Player Score : " + playerScore + " | Computer Score : " + computerScore);
+}
+
+game();
