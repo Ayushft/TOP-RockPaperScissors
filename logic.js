cae2bffd3fce61eb35c2f9e80a3906733d6d7255
Ayush Kothari <ayush.kotharift@gmail.com>
2023-03-27 15:26:18 +0530

    added descriptive comments

--- logic.js
+++ logic.js
@@ -3,12 +3,23 @@ let computerScore = 0;
 let gameSheet = [];
 var btn = document.getElementById("startGame");
 
+/* 
+Function to Get Computer Choice between "Rock", "Paper" and "Scissors"
+- will return a string between (rock, paper, scissors) randomly and in lowerCase
+*/
+
 function getComputerChoice(){
     let options = ["Rock","Paper","Scissors"];
     const randIndex =  Math.floor(Math.random() * options.length);
     const randChoice = options[randIndex];
     return randChoice
 }
+
+/* 
+Function to play a round of "Rock, Paper, Scissors" between user and computer
+- will prompt the user for a choice between : (rock, paper, scissors) and is case-insensitive
+- will increment playerScore/computerScore depending on the winner and return a string with round details
+*/
 
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
@@ -38,6 +49,13 @@ function playRound(playerSelection, computerSelection){
     
 }
 
+/* 
+Function to play a game of "Rock, Paper and Scissors" where 5 rounds are played
+- will prompt the user for a choice between : (rock, paper, scissors) and is case-insensitive subsequently until 5 rounds are completed
+- will increment gameCount to track number of rounds played to reach a combined score of 5
+*/
+
+
 function game(){
     let gameCount = 0;
     while(playerScore + computerScore != 5){
@@ -48,15 +66,21 @@ function game(){
     }
     let playerScoreShow = playerScore;
     let computerScoreShow = computerScore;
+    reset();
     alert(`Player Score : ${playerScoreShow} | Computer Score : ${computerScoreShow}`);
     console.log(gameSheet);
-    reset();
     return("Player Score : " + playerScoreShow + " | Computer Score : " + computerScoreShow);
 }
+
+/* 
+Function to reset scores and gameSheet which contains round details
+*/
+
 
 function reset(){
     computerScore = 0;
     playerScore = 0;
+    gameSheet = [];
 }
 
 btn.addEventListener("click", game);
